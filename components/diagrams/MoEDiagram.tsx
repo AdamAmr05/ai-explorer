056e676cda28922ff8199e963e2824a74afc6abf
AdamAmr05 <adamamr@icloud.com>
2025-05-22 23:13:30 +0300

    Fix MoE diagram layout - remove messy connector lines, keep original content and colors

--- components/diagrams/MoEDiagram.tsx
+++ components/diagrams/MoEDiagram.tsx
@@ -1,41 +1,22 @@
 import React from 'react';
-import { DiagramBlock, Arrow, ConnectorLine } from '../DiagramCommon';
+import { DiagramBlock, Arrow, PlusIcon } from '../DiagramCommon';
 
 export const MoEDiagram: React.FC = () => {
   return (
     <div className="flex flex-col items-center space-y-4 p-4 bg-slate-900/50 rounded-lg text-xs">
       <DiagramBlock title="Input Token" color="green-700" />
       <Arrow direction="down" />
-      <DiagramBlock title="Gating Network" color="green-700" />
+      <DiagramBlock title="Gating Network" details="Selects Top K Experts" color="green-700" />
       
-      {/* Connecting Gating Network to Experts */}
-      <div className="relative w-full h-8 my-2">
-        {/* Main vertical line down from Gating Network */}
-        <ConnectorLine type="vertical" lengthClass="h-4" className="absolute top-[-16px] left-1/2 -translate-x-1/2"/>
-        {/* Horizontal dispatch line */}
-        <ConnectorLine type="horizontal" lengthClass="w-4/5 max-w-xs" className="absolute top-[0px] left-1/2 -translate-x-1/2"/>
-        {/* Branching lines to where experts will be */}
-        <ConnectorLine type="vertical" lengthClass="h-4" className="absolute top-[0px] left-[20%] -translate-x-1/2 transform rotate-0"/>
-        <ConnectorLine type="vertical" lengthClass="h-4" className="absolute top-[0px] left-1/2 -translate-x-1/2 transform rotate-0"/>
-        <ConnectorLine type="vertical" lengthClass="h-4" className="absolute top-[0px] left-[80%] -translate-x-1/2 transform rotate-0"/>
+      <Arrow direction="down" label="Routes to Selected Experts" />
+      
+      <div className="flex justify-around w-full space-x-4">
+        <DiagramBlock title="Expert 1" details="e.g., FFN" color="green-500" />
+        <DiagramBlock title="Expert 2" details="e.g., FFN" color="green-500" />
+        <DiagramBlock title="Expert N" details="e.g., FFN" color="green-500" />
       </div>
 
-      <div className="flex justify-around w-full space-x-2">
-        <DiagramBlock title="Expert 1" color="green-700" />
-        <DiagramBlock title="Expert 2" color="green-700" />
-        <DiagramBlock title="Expert N" color="green-700" />
-      </div>
-
-      {/* Connecting Experts to Weighted Sum */}
-       <div className="relative w-full h-8 my-2">
-        <ConnectorLine type="vertical" lengthClass="h-4" className="absolute top-[-16px] left-[20%] -translate-x-1/2 transform rotate-0"/>
-        <ConnectorLine type="vertical" lengthClass="h-4" className="absolute top-[-16px] left-1/2 -translate-x-1/2 transform rotate-0"/>
-        <ConnectorLine type="vertical" lengthClass="h-4" className="absolute top-[-16px] left-[80%] -translate-x-1/2 transform rotate-0"/>
-        <ConnectorLine type="horizontal" lengthClass="w-4/5 max-w-xs" className="absolute top-[0px] left-1/2 -translate-x-1/2"/>
-        <ConnectorLine type="vertical" lengthClass="h-4" className="absolute top-[0px] left-1/2 -translate-x-1/2"/>
-      </div>
-      
-      <Arrow direction="down" label="Combine Outputs (Weighted by Gating Network)" />
+      <Arrow direction="down" label="Weighted Combination" />
       <DiagramBlock title="Output Token Representation" color="green-700" />
     </div>
   );
